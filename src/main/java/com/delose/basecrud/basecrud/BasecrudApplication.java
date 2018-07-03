725502bc51c698690689fb97dee982fc1f43f29f
delossantos.eugene <delossantos.eugene@heb.com>
2018-07-03 18:57:51 +0800

    init commit

---
+++ src/main/java/com/delose/basecrud/basecrud/BasecrudApplication.java
@@ -0,0 +1,12 @@
+package com.delose.basecrud.basecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasecrudApplication.class, args);
+	}
+}
